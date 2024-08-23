eb578441cef490921177fea852e8b9aa98d5f06b
Yasser Esam <yaaser10esam1010@gmail.com>
2024-08-23 22:39:40 +0300

    style: complete webpage design with responsive layout and styled component

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ window.onload = function() {
         document.getElementById('continue-btn').style.display = 'inline-block';
     }, 2000);
 };
+
+
+let keyboard = document.getElementById('keyboard');
+let qwertyLayout = "QWERTYUIOPASDFGHJKLZXCVBNM";
+const getRandomWord = () => {
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+for (let i = 0; i < qwertyLayout.length; i++) {
+    let button = document.createElement('button');
+    button.textContent = qwertyLayout[i];
+    keyboard.appendChild(button);
+}
